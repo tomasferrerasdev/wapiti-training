4cdec67aac17b5de03ef65bf51e64e771b1d5270
Tomas Ferreras <hellotomasdev@gmail.com>
2024-05-27 20:02:48 -0300

    feat: config tailwind theme and add custom fonts

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
-interface Props {
-  name: string;
-}
-
-export default function App({ name }: Props) {
+export default function App() {
   return (
     <View>
-      <Text>HELLO WAPITI</Text>
+      <Text className="font-uberBold">HELLO WAPITI</Text>
       <StatusBar style="auto" />
     </View>
   );
